7fc906c79a2a7d97a39e3f09c8aefd3740f19f03
Everdrought <everdrought@gmail.com>
2018-10-27 15:23:59 +0200

    Finished first exercise

---
+++ chap04a.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your middle name?"
+second_name = gets.chomp
+puts "What is your last name?"
+third_name = gets.chomp
+puts "Greetings #{first_name} #{second_name} #{third_name}!"
