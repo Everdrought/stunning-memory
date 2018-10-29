d79b94ebc15cac0238f805053e5d6e4df1c39e92
Everdrought <everdrought@gmail.com>
2018-10-29 23:02:50 +0100

    Commited 007

---
+++ chap007.rb
@@ -0,0 +1,8 @@
+toc = ["Table of Contents","Chapter 1: Getting Started", "page 1", "Chapter 2: Numbers", "page 9", "Chapter 3: Letters", "page 13"]
+line_width = 60
+i = 0
+puts(toc[0].center(line_width))
+puts ""
+until i == 6
+    puts(toc[i += 1].ljust(line_width/2) + toc[i += 1].rjust(line_width/2))
+end
