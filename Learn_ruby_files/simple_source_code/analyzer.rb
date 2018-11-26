54682dda02cf54c256a374e867a3a07988cfbefe
Everdrought <everdrought@gmail.com>
2018-11-26 15:46:38 +0100

    Exercise complete

--- Learn_ruby_files/simple_source_code/analyzer.rb
+++ Learn_ruby_files/simple_source_code/analyzer.rb
@@ -1,7 +1,15 @@
-line_count=0
-File.open("text.txt").each do |line| 
-    line_count+=1
-    text << line
-end
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+total_nospace_characters = text.gsub(/\s+/,"")
+total_sentences = text.split(/\.|\?|!/).length
+total_paragraphs = text.split(/\n\n/).length
+
 
 puts "#{line_count} lines"
+puts "#{total_nospace_characters.length} characters"
+puts "#{text.split.length} words"
+puts "#{total_sentences} sentences"
+puts "#{total_paragraphs} paragraphs"
+puts "\n#{text.split.length / total_sentences} words per sentence average"
+puts "#{total_sentences / total_paragraphs} sentences per paragraph average"
