2938262f88978e05186d9de3b2c6d28d4d43e2d4
Everdrought <everdrought@gmail.com>
2018-11-27 21:52:25 +0100

    Pstore now creates a file and saves objects

--- Learn_ruby_files/simple_source_code/pstore.rb
+++ Learn_ruby_files/simple_source_code/pstore.rb
@@ -1,3 +1,23 @@
 class Person
     attr_accessor :name, :job, :gender, :age
 end
+
+fred = Person.new
+fred.name = "Fred"
+fred.job = "Janitor"
+fred.gender = "Male"
+fred.age = "19"
+
+steve = Person.new
+steve.name = "Steve"
+steve.job = "Janitor"
+steve.gender = "Male"
+steve.age = "41"
+
+require 'pstore'
+store = PStore.new("storagefile.txt")
+store.transaction do
+    store[:people] ||=Array.new
+    store[:people] << fred
+    store[:people] << steve
+end
