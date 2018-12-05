cb59a6b04da8cd6ca9fbc08ba3c05afaf4f4d970
Everdrought <everdrought@gmail.com>
2018-12-05 18:47:18 +0100

    Testing complete

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -1,5 +1,33 @@
 class Calculator
-    def add a, b
-    a+b
+    def add *num
+        answer = 0
+        num.each do |i|
+            answer+=i
+        end
+        return answer
+    end
+    def subtract *num
+        answer = num[0]
+        num.each do |i|
+            next if i == num[0]
+            answer-=i
+        end
+        return answer
+    end
+    def multiply *num
+        answer = num[0]
+        num.each do |i|
+            next if i == num[0]
+            answer*=i
+        end
+        return answer
+    end
+    def divide *num
+        answer = num[0]
+        num.each do |i|
+            next if i == num[0]
+            answer/=i
+        end
+        return answer
     end
 end
--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -4,9 +4,47 @@ require "./lib/calculator"
 RSpec.describe Calculator do
     describe "#add" do
         it "returns the sum of two numbers" do
-        calculator = Calculator.new
-        expect(calculator.add(5, 2)).to eql(7)
+            calculator = Calculator.new
+            expect(calculator.add(5, 2)).to eql(7)
+        end
+
+        it "returns the sum of more than two numbers" do
+            calculator = Calculator.new
+            expect(calculator.add(5,2,1)).to eql(8)
         end
     end
-end
 
+    describe "#multiply" do
+        it "returns the result of two numbers multiplied" do
+            calculator = Calculator.new
+            expect(calculator.multiply(5,2)).to eql(10)
+        end
+        it "returns the result of more than two numbers multiplied" do
+            calculator = Calculator.new
+            expect(calculator.multiply(5,2,3)).to eql(30)
+        end
+    end
+
+    describe "#subtract" do
+        it "return the subtracted number" do
+            calculator = Calculator.new
+            expect(calculator.subtract(5,2)).to eql(3)
+        end
+        it "returns the result of more than two numbers subtracted" do
+            calculator = Calculator.new
+            expect(calculator.subtract(6,2,3)).to eql(1)
+        end
+    end
+
+    describe "#divide" do
+        it "returns the result of two numbers divided" do
+            calculator = Calculator.new
+            expect(calculator.divide(6,2)).to eql(3)
+        end
+        it "returns the result of more than two numbers divided" do
+            calculator = Calculator.new
+            expect(calculator.divide(12,2,3)).to eql(2)
+        end
+    end
+
+end
